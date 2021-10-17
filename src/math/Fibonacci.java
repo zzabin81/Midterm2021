e66a8de7a248a13afa24d9b44da9ab5baec88f6c
Ze Be Zabin <zzabin81@gmail.com>
2021-10-17 17:46:35 -0400

    Fibonacci

--- src/math/Fibonacci.java
+++ src/math/Fibonacci.java
@@ -4,6 +4,14 @@ import java.util.Arrays;
 
 public class Fibonacci {
     public static void main(String[] args) {
+        int prev = 0;
+        int next = 1;
+        for (int i = 0; i < 40; i++){
+            System.out.println(prev);
+            prev = next - prev;
+            next = next + prev;
+
+        }
          /*
           Write 40 Fibonacci numbers with java.
          */
