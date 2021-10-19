aaf858d59ade69fd77049559b51c2ce61e217397
Ze Be Zabin <zzabin81@gmail.com>
2021-10-19 10:45:51 -0400

    LowestNumber

--- src/math/LowestNumber.java
+++ src/math/LowestNumber.java
@@ -12,11 +12,26 @@ public class LowestNumber {
 		 * Write java solution to find the lowest number from this array.
 		 * Use one of the databases from mysql or mongodb to store and to retrieve.
 		 */
-
-
 		//implementation here...
-
-
+		//an array
+		int[] arr = {33, 67, 22, 59, 15, 44};
+		//initialize with the largest possible value
+		int lowest = Integer.MAX_VALUE;
+		//find the lowest number of array
+		int i = 0;
+		    while(i<arr.length) {
+			//check if the lowest number is greater than element
+			if(lowest>arr[i]) {
+				//update the lowest number
+				lowest=arr[i];
+			}
+			i++;
+		}
+		System.out.println("The lowest number is : "+ lowest);
 	}
-
 }
+
+
+
+
+
