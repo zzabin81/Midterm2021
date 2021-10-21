057b765f254a8a3ca744bef85c9d72b1654159e7
Ze Be Zabin <zzabin81@gmail.com>
2021-10-21 11:46:53 -0400

    UseArrayList

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,7 +1,10 @@
 package datastructure;
 
+import databases.ConnectDB;
+
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -17,6 +20,49 @@ public class UseArrayList {
 
         //implementation here
 
+		List<Integer> ar = new ArrayList<>();
+
+		ar.add(4444);
+		ar.add(2222);
+		ar.add(9999);
+		ar.add(8888);
+		ar.add(2121);
+		ar.add(6565);
+		ar.add(9000);
+		ar.add(1888);
+		//Print Array List
+		System.out.println("The elements of the ArrayList are: " + ar);
+
+		//Using get method to peek the first element
+		System.out.println("Arrays peek element: " + ar.get(0));
+
+		//remove element from ArrayList
+		System.out.println("Remove element at index 3: " + ar.remove(3));
+
+		//Retrieving elements from ArrayList
+		System.out.println("Re-retrieving elements from ArrayList: " + ar);
+
+		// retrieving data Using forEach loop
+		System.out.print("Re-retrieving elements from ArrayList: ");
+		for (Integer in : ar) {
+			System.out.print(" " + in);
+		}
+		System.out.println();
+
+		//retrieving data using iterator
+		System.out.print("Re-retrieving elements from ArrayList: ");
+		Iterator it = ar.iterator();
+		while (it.hasNext()) {
+			Integer i = (Integer) it.next();
+			System.out.print(" " + i);
+		}
+
+		System.out.println();
+
+		Collections.sort(ar);
+		System.out.println(ar);
+		List list = new ArrayList();
+		list = ar;
 
 		//connect to db
 	
