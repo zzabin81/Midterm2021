0a60c3fce40f6d99e7f0fe95a1f812b547707871
Ze Be Zabin <zzabin81@gmail.com>
2021-10-21 11:50:16 -0400

    UseQueue

--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -10,10 +10,36 @@ public class UseQueue {
 		/*
 		 * Demonstrate how to use Queue that includes add,peek,remove,pool elements.
 		 * Use For Each loop and while loop with Iterator to retrieve data.
-		 * 
+		 *
 		 */
 
 		//implementation here
+
+		Queue<String> queue = new LinkedList<>();
+		queue.add("school");
+		queue.add("academy");
+		queue.add("education");
+		queue.add("university");
+		queue.add("campus");
+
+		queue.remove("school");
+		queue.add("college");
+
+		System.out.println(queue.peek());
+		System.out.println(queue.poll());
+		System.out.println(" ");
+		System.out.println("while loop method to retrieve data: ");
+		Iterator itrat = queue.iterator();
+		while (itrat.hasNext()) {
+			String z = (String) itrat.next();
+			System.out.println(z);
+		}
+		System.out.println(" ");
+		System.out.println("for each loop method to retrieve data: ");
+		for (String a : queue) {
+			System.out.println(a);
+		}
+
 
 	}
 
