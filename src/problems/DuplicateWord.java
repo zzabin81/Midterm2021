217e408b45e1e09a7f2327804177efc2be8e250d
Ze Be Zabin <zzabin81@gmail.com>
2021-10-21 20:42:57 -0400

    DuplicateWord

--- src/problems/DuplicateWord.java
+++ src/problems/DuplicateWord.java
@@ -1,7 +1,10 @@
-package problems;
+package problems;//done
 
-import java.util.HashMap;
-import java.util.Map;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by mrahman on 04/22/17.
@@ -9,15 +12,45 @@ import java.util.Map;
 public class DuplicateWord {
 
     public static void main(String[] args) {
-        //Using main method to execute java code
         /*
          * Write a java program to find the duplicate words and their number of occurrences in the string.
          * Also Find the average length of the words.
          */
 
+        String string = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+
         //implementation here...
 
+        Set<String> duplicates = duplicateWords(string);
+        System.out.println("In this scenario duplicate words are " + duplicates);
 
+    }
+    public static Set<String> duplicateWords(String s) {
+
+        DecimalFormat df = new DecimalFormat(".00");
+
+        if (s == null || s.isEmpty()) {
+            return Collections.emptySet();
+        }
+        Set<String> duplicateWords = new HashSet<>();
+        String[] array = s.split(" ");
+        double avgLength = (double) s.length() / array.length;
+
+        Set<String> set = new HashSet<>();
+        ArrayList<String> arraylist = new ArrayList<String>();
+        for (String word : array) {
+            arraylist.add(word);
+            if (!set.add(word)) {
+                duplicateWords.add(word);
+            }
+        }
+        for (String word : duplicateWords) {
+            if (Collections.frequency(arraylist, word) > 1) {
+                System.out.println("'" + word +"' occurs " + Collections.frequency(arraylist, word) + " times.");
+            }
+        }
+        System.out.println("Average length of these words is: " + df.format(avgLength));
+        return duplicateWords;
     }
 
 }
