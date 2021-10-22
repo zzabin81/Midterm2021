ef38a213ab2bf02a19f21992d23f6563536f9eea
Ze Be Zabin <zzabin81@gmail.com>
2021-10-22 09:22:49 -0400

    ProcessStudentInfo

--- src/parser/ProcessStudentInfo.java
+++ src/parser/ProcessStudentInfo.java
@@ -1,6 +1,5 @@
-package parser;
+package parser; //done
 
-import databases.ConnectDB;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -12,69 +11,82 @@ import java.util.Map;
 
 public class ProcessStudentInfo {
 
-		/*
-		 * Under XmlReader class, the parseData() will return list of Student Info which will contain Student first name, last name and score.
-		 * You need to implement the method name "convertIntToChar()" which will convert String score into char Grade.('A'for 90 to 100,'B'for 80 to 89 and 
-		 * 'C' for 70 to 79.
-		 * 
-		 * Here in the main method fill in the code that outlined to read xml data parsed into 2 separate ArrayList, then store into map. 
-		 * Once map has all data, retrieve those data and out put to console.
-		 *
-		 * Do any necessary steps that require for below successful output.
-		 * ......................................................
-		 * Student (id= 1) "Ibrahim" "Khan"        		Grade= B
-		 * Student (id= 2) "Asif" "Roni"          		Grade= A
-		 * Student (id= 3) "Gumani" "Hose"              Grade= A
-		 * Student (id= 4) "Sukanto" "Shaha"            Grade= B
-		 * Student (id= 5) "MD" "Hossain"               Grade= C
-		 * ......................................................
-		 * 
-		 *
-		 * Use any databases[MongoDB, Oracle, MySql] to store data and to retrieve data.
-		 *
-		 */
+	/*
+	 * Under XmlReader class, the parseData() will return list of Student Info which will contain Student first name, last name and score.
+	 * You need to implement the method name "convertIntToChar()" which will convert String score into char Grade.('A'for 90 to 100,'B'for 80 to 89 and
+	 * 'C' for 70 to 79.
+	 *
+	 * Here in the main method fill in the code that outlined to read xml data parsed into 2 separate ArrayList, then store into map.
+	 * Once map has all data, retrieve those data and out put to console.
+	 *
+	 * Do any necessary steps that require for below successful output.
+	 * ......................................................
+	 * Student (id= 1) "Ibrahim" "Khan"        		Grade= B
+	 * Student (id= 2) "Asif" "Roni"          		Grade= A
+	 * Student (id= 3) "Gumani" "Hose"              Grade= A
+	 * Student (id= 4) "Sukanto" "Shaha"            Grade= B
+	 * Student (id= 5) "MD" "Hossain"               Grade= C
+	 * ......................................................
+	 *
+	 *
+	 * Use any databases[MongoDB, Oracle, MySql] to store data and to retrieve data.
+	 *
+	 */
 
-			public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
-				//Path of XML data to be read.
-
-
-                //Create ConnectDB Object
-
-
-				//Declare a Map with List<String> into it.
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+		//Path of XML data to be read.
+		String pathSelenium = System.getProperty("user.dir") + "/src/parser/selenium.xml";
+		String pathQtp = System.getProperty("user.dir") + "/src/parser/qtp.xml";
+		String tag = "id";
 
 
-				/*Declare 2 ArrayList with Student data type to store Selenium student into one of the ArrayList and
+		//Declare a Map with List<String> into it.
+		Map<String, List<Student>> list = new LinkedHashMap<String, List<Student>>();
+
+		/*Declare 2 ArrayList with Student data type to store Selenium student into one of the ArrayList and
 				  Qtp student into another ArrayList. */
-
-				
-				//Parse Data using parseData method and then store data into Selenium ArrayList.
-
-
-				//Parse Data using parseData method and then store data into Qtp ArrayList.
-				
-				//add Selenium ArrayList data into map.
-			
-				//add Qtp ArrayList data into map.
-		
-		      	
-				//Retrieve map data and display output.
+		List<Student> seleniumStudents = new ArrayList<Student>();
+		List<Student> qtpStudents = new ArrayList<Student>();
 
 
+		//Parse Data using parseData method and then store data into Selenium ArrayList.
+		XmlReader xmlReader = new XmlReader();
+		seleniumStudents = xmlReader.parseData(tag, pathSelenium);
 
-				//Store Qtp data into Qtp table in Database
+		//Parse Data using parseData method and then store data into Qtp ArrayList.
+		qtpStudents = xmlReader.parseData(tag, pathQtp);
+		//add Selenium ArrayList data into map.
+		list.put("Selenium Students", seleniumStudents);
+		//add Qtp ArrayList data into map.
+		list.put("QTP Students", qtpStudents);
 
-
-				//connectDB.insertDataFromArrayListToMySql(seleniumStudents, "qtp","studentList");
-
-				//Store Selenium data into Selenium table in Database
-
-				//Retrieve Qtp students from Database
-
-
-			   //Retrieve Selenium students from Database
-
-
+		//Retrieve map data and display output.
+		for (Map.Entry<String, List<Student>> value : list.entrySet()) {
+			List<Student> students = list.get(value.getKey());
+			System.out.println("\n" + value.getKey() + " Students");
+			for (Student s : students) {
+				String id = s.getId();
+				String firstname = s.getFirstName();
+				String lastname = s.getLastName();
+				String grade = s.getScore();
+				System.out.println("Student (id=" + id + ") '" + firstname + "'  '" + lastname + "       \t'Grade= " + grade);
 			}
+		}
 
+		//Store Qtp data into Qtp table in Database
+
+
+		//connectDB.insertDataFromArrayListToMySql(seleniumStudents, "qtp","studentList");
+
+		//Store Selenium data into Selenium table in Database
+
+		//Retrieve Qtp students from Database
+
+
+		//Retrieve Selenium students from Database
+
+
+
+
+	}
 }
